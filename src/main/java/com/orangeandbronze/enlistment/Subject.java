46b2988b68353198cda7002ea6e25b837dd41b68
calen <calen@orangeandbronze.com>
2021-11-13 14:41:06 +0800

    Subject has alphanumeric section ID

--- src/main/java/com/orangeandbronze/enlistment/Subject.java
+++ src/main/java/com/orangeandbronze/enlistment/Subject.java
@@ -1,4 +1,35 @@
 package com.orangeandbronze.enlistment;
 
+import static org.apache.commons.lang3.StringUtils.isAlphanumeric;
+import static org.apache.commons.lang3.Validate.*;
+
 class Subject {
+
+    private final String subjectId;
+
+    Subject(String subjectId) {
+        notBlank(subjectId);
+        isTrue(isAlphanumeric(subjectId), "subjectId must be alphanumeric, was: " + subjectId);
+        this.subjectId = subjectId;
+    }
+
+    @Override
+    public String toString() {
+        return subjectId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Subject subject = (Subject) o;
+
+        return subjectId != null ? subjectId.equals(subject.subjectId) : subject.subjectId == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return subjectId != null ? subjectId.hashCode() : 0;
+    }
 }
