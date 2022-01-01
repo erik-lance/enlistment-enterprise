aa59bffac334dde70e7e7ccaa45d6bbf85cec76b
calen <calen@orangeandbronze.com>
2022-01-01 13:57:29 +0800

    Replaced sectionId declaration with DEFAULT_SECTION_ID

--- src/test/java/com/orangeandbronze/enlistment/controllers/EnlistControllerTest.java
+++ src/test/java/com/orangeandbronze/enlistment/controllers/EnlistControllerTest.java
@@ -18,12 +18,11 @@ class EnlistControllerTest {
     void enlist_student_in_section() {
         // Given the Controller w/ a student in session, param of sectionId to enlist, & UserAction "ENLIST"
         Student student = mock(Student.class);
-        String sectionId = "X";
         UserAction userAction = UserAction.ENLIST;
         // When enlist (post) method is called
         SectionRepository sectionRepository = mock(SectionRepository.class);
         Section section = newDefaultSection();
-        when(sectionRepository.findById(sectionId)).thenReturn(Optional.of(section));
+        when(sectionRepository.findById(DEFAULT_SECTION_ID)).thenReturn(Optional.of(section));
         StudentRepository studentRepository = mock(StudentRepository.class);
         EnlistController controller = new EnlistController();
         controller.setSectionRepo(sectionRepository);
@@ -32,10 +31,10 @@ class EnlistControllerTest {
         Session session = mock(Session.class);
         when(entityManager.unwrap(Session.class)).thenReturn(session);
         controller.setEntityManager(entityManager);
-        String returnVal = controller.enlistOrCancel(student, sectionId, userAction);
+        String returnVal = controller.enlistOrCancel(student, DEFAULT_SECTION_ID, userAction);
         // Then
         // - retrieve the Section object from the DB using the sectionId
-        verify(sectionRepository).findById(sectionId);
+        verify(sectionRepository).findById(DEFAULT_SECTION_ID);
         // - student.enlist method will be called, passing in the section
         verify(student).enlist(section);
         // - save student to DB
