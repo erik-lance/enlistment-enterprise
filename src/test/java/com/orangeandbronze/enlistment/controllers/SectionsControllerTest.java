ff68491a0b4c54c9c38beb27403e85876f9e3940
calen <calen@orangeandbronze.com>
2022-09-09 14:59:14 +0800

    Added Given-When-Then comments

--- src/test/java/com/orangeandbronze/enlistment/controllers/SectionsControllerTest.java
+++ src/test/java/com/orangeandbronze/enlistment/controllers/SectionsControllerTest.java
@@ -16,7 +16,13 @@ class SectionsControllerTest {
 
     @Test
     void createSection_save_new_section_to_repository() {
-
+        // Given the controller, repositories & valid parameter arguments for creating a section
+        // When the controller receives the arguments
+        // Then
+        // - it should retrieve the entities from the db, create a new section
+        // - save the section in the db
+        // - set a flash attribute called "sectionSuccessMessage" with the message "Successfully created new section " + sectionId
+        // - return the string value "redirect:sections" to redirect to the GET method
 
     }
 }
