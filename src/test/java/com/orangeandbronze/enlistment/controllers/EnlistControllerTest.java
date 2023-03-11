752475725aa0fc2cc9bfd1e3a021f707de4d54d0
erik-lance <eriklance@gmail.com>
2023-03-11 18:02:49 +0800

    Added commit from mob coding session last Feb 21

--- src/test/java/com/orangeandbronze/enlistment/controllers/EnlistControllerTest.java
+++ src/test/java/com/orangeandbronze/enlistment/controllers/EnlistControllerTest.java
@@ -16,7 +16,13 @@ class EnlistControllerTest {
 
     @Test
     void enlist_student_in_section() {
-
+        // Given a student & the sectionId of the section that the student wants to enroll in
+        EnlistController controller = new EnlistController();
+        Student student = newDefaultStudent();
+        SectionRepository sectionRepository = null;
+        controller.setSectionRepo(sectionRepository);
+        // When the parqmeters are received in the POST method
+        controller.enlistOrCancel(student, DEFAULT_SECTION_ID, UserAction.ENLIST);
     }
 
 }
